7605dbe2038e4ff5b4748ca1c097d9916b9dcf6f
patkahnke <pat.kahnke@banno.com>
2018-09-10 19:12:53 -0500

    creates Comments.js

---
+++ client/components/Comments.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Comments = React.createClass({
+    render() {
+        return (
+            <div>
+                I am the eggman
+            </div>
+        )
+    }
+});
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
     render() {
+        console.log('this.props.params:', this.props.params);
+        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        const post = this.props.posts[i];
         return (
             <div className="single-photo">
-                I'm the Single Photo
+                <Photo i={i} post={post} {...this.props} />
+                <Comments />
             </div>
         );
     }
